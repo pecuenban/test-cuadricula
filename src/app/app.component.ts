413f9f46cb972ffce54cf110be9c76cf92731b8b
pecuenban <pedrocuenca95@yahoo.es>
2021-09-10 00:21:38 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,9 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
   url;
   imagenes = [];
-  cantidad = 10;
+  cantidad = 15;
   alto = 5;
+  bloques_ancho = 5;
   ngOnInit() {
     this.http
       .get(
@@ -21,16 +22,34 @@ export class AppComponent implements OnInit {
           this.cantidad
       )
       .subscribe(value => {
+        let linea = this.bloques_ancho;
         this.conversion(value).forEach(element => {
+          console.log('linea: ' + linea);
+          let empezar = false;
+          let anch = linea - this.getRandomInt(1, this.bloques_ancho - 1);
+          console.log('anch: ' + anch);
+          if (anch < 1) {
+            anch = linea;
+            empezar = true;
+          }
+          console.log('anch 2: ' + anch);
           let imagen = {
             url: element.urls.small,
             alto: 1,
-            ancho: 1,
+            ancho: anch,
             titulo: element.user.name
           };
+          if (empezar) {
+            linea = this.bloques_ancho;
+          } else {
+            linea = linea - anch;
+          }
           this.imagenes.push(imagen);
         });
       });
+  }
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
   conversion(data) {
