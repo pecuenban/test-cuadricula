030758f9055896adc74b1297f95a381a2043ffd5
pecuenban <pedrocuenca95@yahoo.es>
2021-09-18 17:31:47 +0200

    Visualizar fotos en grande

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
@@ -12,32 +12,33 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
   url;
   imagenes = [];
-  cantidad = 15;
+  cantidad = 5;
   alto = 5;
   bloques_ancho = 5;
-  ngOnInit() {
+  cargarImagenes() {
     this.http
       .get(
         'https://api.unsplash.com/photos/random?client_id=xk4oMBk4AZMJfcdCl_T0h2PNVerED0fdauHF6SxqgKg&count=' +
           this.cantidad
       )
-      .subscribe(value => {
+      .subscribe((value) => {
         let linea = this.bloques_ancho;
-        this.conversion(value).forEach(element => {
-          console.log('linea: ' + linea);
+        let contador = 1;
+        this.conversion(value).forEach((element) => {
           let empezar = false;
           let anch = linea - this.getRandomInt(1, this.bloques_ancho - 1);
-          console.log('anch: ' + anch);
-          if (anch < 1) {
+
+          if (anch < 1 || this.cantidad == contador) {
             anch = linea;
             empezar = true;
           }
-          console.log('anch 2: ' + anch);
+
           let imagen = {
             url: element.urls.small,
             alto: 1,
             ancho: anch,
-            titulo: element.user.name
+            titulo: element.user.name,
+            pulsado: false,
           };
           if (empezar) {
             linea = this.bloques_ancho;
@@ -45,8 +46,50 @@ export class AppComponent implements OnInit {
             linea = linea - anch;
           }
           this.imagenes.push(imagen);
+          contador++;
         });
       });
+  }
+  activo = -1;
+  pulsar(indice) {
+    if (this.activo == -1) {
+      this.imagenes[indice].pulsado = true;
+      this.activo = indice;
+    }
+  }
+  despulsar(evento, indice) {
+    this.imagenes.forEach((element) => {
+      element.pulsado = false;
+    });
+    setTimeout(() => {
+      this.activo = -1;
+    }, 100);
+    /*if (evento) {
+      
+      this.imagenes[indice].pulsado = false;
+      
+    }*/
+  }
+
+  anterior($event, i) {
+    this.imagenes[this.activo].pulsado = false;
+    if (this.activo == 0) {
+      this.activo = this.imagenes.length;
+    }
+    this.imagenes[this.activo - 1].pulsado = true;
+    this.activo -= 1;
+  }
+  siguiente($event, i) {
+    this.imagenes[this.activo].pulsado = false;
+    if (this.activo == this.imagenes.length - 1) {
+      this.activo = -1;
+    }
+    this.imagenes[this.activo + 1].pulsado = true;
+    this.activo += 1;
+  }
+
+  ngOnInit() {
+    this.cargarImagenes();
   }
   getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
--- src/app/tarjeta/tarjeta.component.ts
+++ src/app/tarjeta/tarjeta.component.ts
@@ -1,18 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-tarjeta',
   templateUrl: './tarjeta.component.html',
-  styleUrls: ['./tarjeta.component.css']
+  styleUrls: ['./tarjeta.component.css'],
 })
 export class TarjetaComponent implements OnInit {
-@Input() ancho=1;
-@Input() alto=1;
-@Input() titulo="Titulo";
-@Input() imagen="Titulo";
-  constructor() { }
-
-  ngOnInit() {
+  @Input() ancho = 1;
+  @Input() alto = 1;
+  @Input() titulo = 'Titulo';
+  @Input() imagen = 'Titulo';
+  @Input() pulsado = false;
+  @Output() despulsado = new EventEmitter<boolean>();
+  @Output() siguiente = new EventEmitter<boolean>();
+  @Output() anterior = new EventEmitter<boolean>();
+  constructor() {}
+  cerrar() {
+    this.despulsado.emit(true);
   }
+  ngOnInit() {}
 
+  ant() {
+    this.anterior.emit(true);
+  }
+  sig() {
+    this.siguiente.emit(true);
+  }
 }
